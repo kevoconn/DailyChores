4351c2f32b805081cfb65c16001ae9e63eae8bed
github_kevoconn <kcoconnor84@gmail.com>
2022-11-22 13:58:01 -0600

    added color coding, save function

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
-
-
-
 $(function () {
   let buttons = $(".saveBtn");
 
@@ -16,55 +8,43 @@ $(function () {
     localStorage.setItem(parent, description);
   });
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  let today = dayjs();
-  $('#currentDay').text(today.format('MMM D, YYYY'));
-    
-$("time-block").each(function(){
-$(this).removeClass("future");
-$(this).removeClass("past");
-$(this).removeClass("present");
+  let date = dayjs();
+  $("#currentDay").text(date.format("MMM D, YYYY"));
 
-let hour = parsInt($(this).attr("id").split("-")[1]);
+  $(".time-block").each(function () {
+    let hour = parseInt($(this).attr("id").split("-")[1]);
 
-
-
-if (hour < dayjs().hour()){
-    $(this).addClass("past");
-}else if (hour === dayjs().hour()){
-    $(this).addClass("present");
-}else {
-        $(this).addClass("future");
+    if (hour < dayjs().hour()) {
+      $(this).addClass("past");
+    } else if (hour === dayjs().hour()) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
     }
-console.log()
+    console.log();
+  });
 });
 
+$("#hour-9").children(".description").val(localStorage.getItem("hour-9"));
+$("#hour-10").children(".description").val(localStorage.getItem("hour-10"));
+$("#hour-11").children(".description").val(localStorage.getItem("hour-11"));
+$("#hour-12").children(".description").val(localStorage.getItem("hour-12"));
+$("#hour-13").children(".description").val(localStorage.getItem("hour-13"));
+$("#hour-14").children(".description").val(localStorage.getItem("hour-14"));
+$("#hour-15").children(".description").val(localStorage.getItem("hour-15"));
+$("#hour-16").children(".description").val(localStorage.getItem("hour-16"));
+$("#hour-17").children(".description").val(localStorage.getItem("hour-17"));
 
-  })
-$('hour-9').children('description').val(localStorage.getItem('hour-9'));
-$('hour-10').children('description').val(localStorage.getItem('hour-10'));
-$('hour-11').children('description').val(localStorage.getItem('hour-11'));
-$('hour-12').children('description').val(localStorage.getItem('hour-12'));
-$('hour-13').children('description').val(localStorage.getItem('hour-13'));
-$('hour-14').children('description').val(localStorage.getItem('hour-14'));
-$('hour-15').children('description').val(localStorage.getItem('hour-15'));
-$('hour-16').children('description').val(localStorage.getItem('hour-16'));
-$('hour-17').children('description').val(localStorage.getItem('hour-17'));
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+/*GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist*/
